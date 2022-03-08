c93f38565ea1de043fd95ccd82f0b94be8f228f7
yue-studio <47492421+yue-studio@users.noreply.github.com>
2022-03-08 08:03:59 -0800

    Update substring.c
    added some debugging code

--- substring.c
+++ substring.c
@@ -13,16 +13,21 @@ int substrings(char *string) {
     int res = 0;
     int visited[256];
     
-    for (i=0; i < len; i++) {
-        for (j=i+1; j < len; j++) {
+    for (i = 0; i < len; i++) {
+        for (j = i; j < len; j++) {
+            printf("%d %d checking %c \n ", i,j,string[j]);
             if (visited[string[j]] == 1) {
+                printf("visited\n");
+                printf(" break\n");
                 break;
             } else {
-                res = MAX(res, j - i);
+                res = MAX(res, j - i + 1);
                 visited[string[j]] = 1;
+                printf("unique, set visited %c to 1, length: %d\n", string[j], res);
             }
         }
         visited[string[i]] = 0;
+        printf("clearing %c\n", string[i]);
     }
     return res;
 }
